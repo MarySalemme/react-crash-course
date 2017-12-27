1e7e598000c03049c757d9686d316b89183ed78f
MarySalemme <marysalemme1@gmail.com>
2017-12-27 15:34:35 +0100

    Props

--- exercises/02-props/index.js
+++ exercises/02-props/index.js
@@ -3,13 +3,17 @@ import React from 'react';
 class Book extends React.Component {
   render() {
     return (<div>
-      {/* TODO use the properties to render the name of the book and the read status */}
+      <p>{this.props.title}</p>
+      <input type="checkbox" checked={this.props.read}/> Read
     </div>);
   }
 }
 
 // TODO
-Book.defaultProps = {};
+Book.defaultProps = {
+  title: '',
+  read: false
+};
 
 class Library extends React.Component {
   render() {
@@ -19,9 +23,9 @@ class Library extends React.Component {
     return (
       <ul>
         {/* don't declare any attributes here */}
-        <li><Book /></li>
+        <li><Book title={title} read={read}/></li>
         {/* TODO pass the title and read values to Book by using the Book tag attributes */}
-        <li><Book /></li>
+        <li><Book title="Harry Potter" read={false} /></li>
       </ul>
     );
   }
